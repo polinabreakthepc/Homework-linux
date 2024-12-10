47a175761582d0ee0d5e1f8f2005b847bab3d648
polinabreakthepc <purazmatova@gmail.com>
2024-12-10 23:31:34 +0300

    2.1.sh

---
+++ 2.1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+option=$1
+word=$2
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]
+then
+    echo "Ошибка:введите аргумент -u или -l и слово"
+    exit 
+fi
+
+if [[$1 == "-l"]]
+then
+    echo "${2,,}"
+elif [[$1 == "-u"]]   
+then
+    echo "${2^^}"
+else
+    echo "Аргумент не найден"
+    exit
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
